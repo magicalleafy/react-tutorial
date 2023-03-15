b67bc3ce4f27172c0f0d0d2191a085c9a2fb71a0
magicalleafy <magicstories@139.com>
2023-03-15 13:09:59 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    characters : [],
+  }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+    
+    this.setState({
+      characters: characters.filter((chatacter, i)=>{
+        return i !== index
+      }),
+    })
+  }
+
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
+  }
+
+  fetchFakeApi = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  render() {
+    const {characters} = this.state
+
+    return(
+      <div className="container">
+        <h1>Hello, Peach!</h1>
+        <input type="button" value="try fake api" onClick={this.fetchFakeApi} />
+        <Table characterData = {characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    )
+  }
+};
 
 export default App;
